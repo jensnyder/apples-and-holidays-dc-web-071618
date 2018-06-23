90184c2ace8a802c13e15666417947ffc4d633c6
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-23 15:47:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      holiday.to_s.split("_")
+      holiday.to_s.split("_").each do |word|
        puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
     end
   end
